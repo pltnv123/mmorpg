6633885966848faaf23fc2a94f6b71016a802ffa
pltnv123 <dotasymphony@gmail.com>
2023-05-15 13:45:41 +0300

    Completed the models and added them to the admin panel

--- mmorpg/appmmo/admin.py
+++ mmorpg/appmmo/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Advertisement, Responses
 
 # Register your models here.
+admin.site.register(Advertisement)
+admin.site.register(Responses)
--- mmorpg/appmmo/models.py
+++ mmorpg/appmmo/models.py
@@ -5,19 +5,38 @@ from django.db import models
 # Create your models here.
 
 class Advertisement(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    tank = 'TK'
+    heal = 'HL'
+    dd = 'DD'
+    trader = 'TD'
+    guild_masters = 'GM'
+    quest_giver = 'KG'
+    blacksmith = 'BS'
+    the_tanner = 'TR'
+    potions_master = 'PM'
+    master_of_spells = 'SP'
 
+    CATEGORY_CHOIESES = (
+        (tank, ('Танки')),
+        (heal, ('Хилы')),
+        (dd, ('ДД')),
+        (trader, ('Торговец')),
+        (guild_masters, ('Гилдмастер')),
+        (quest_giver, ('Квестгивер')),
+        (blacksmith, ('Кузнец')),
+        (the_tanner, ('Кожевник')),
+        (potions_master, ('Зельевар')),
+        (master_of_spells, ('Мастер заклинаний')),
+    )
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     heading = models.CharField(max_length=128, help_text=('Название объявления'))
     text = models.TextField(help_text=('Это текст объявления'))
+    classType = models.CharField(max_length=2, choices=CATEGORY_CHOIESES, help_text=('Тип класса'))
     dateCreation = models.DateTimeField(auto_now_add=True)
 
-
-class Category(models.Model):
-    name = models.CharField(max_length=64, help_text=('Это категория объявления'))
-    postCategory = models.ForeignKey(Advertisement, related_name='postCategory', on_delete=models.CASCADE)
-
     def __str__(self):
-        return self.name.title()
+        return f'{self.heading}: {self.text}. Автор: {self.author}, тип героя: {self.classType}'
 
 
 class Responses(models.Model):
